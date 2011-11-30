8b59e24f57f46cb4a9e22339fb2eacfe3fcb94e8
Jimmy Cappaert <jimmy@cappaert.com>
2011-11-30 23:15:28 +0100

    Split some files and functions

--- autocheckin.php
+++ autocheckin.php
@@ -9,12 +9,12 @@
  * 	<jimmy@cappaert.com>
  */
 
-// Init 
+// Init external libraries 
 
 require_once("lib/fsq/FoursquareAPI.class.php");
-require_once("lib/redis/redis.php");
-require_once("lib/redis/redis.pool.php");
-require_once("lib/redis/redis.peer.php");
+
+// Init proprietary payloads
+
 require_once("includes/functions.php");
 require_once("config/locations.php");
 require_once("config/tokens.php");
@@ -22,19 +22,26 @@ require_once("config/tokens.php");
 // Let's go!
 
 $results = json_decode(getLocation());
-$latitude = $results->location->position->latitude;
-$longitude = $results->location->position->longitude;
 
-// Loop known locations to see if a check-in is needed
+if(!$results->error) {
 
-foreach($locations as $location=>$details) {
+	$latitude = $results->location->position->latitude;
+	$longitude = $results->location->position->longitude;
 
-	if(calcDistance($latitude, $longitude, $details['cords']['latitude'], $details['cords']['longitude']) < 0.1) {
+	if($latitude && $longitude) {
 
-		if(getValue("VENUE_".$location) != date("Ymd", time())) {
+		foreach($locations as $location=>$details) {
 
-			setValue("VENUE_".$location, date("Ymd", time()));
-			doCheckin($location);
+			if(calcDistance($latitude, $longitude, $details['cords']['latitude'], $details['cords']['longitude']) < 0.1) {
+
+				if(getValue("VENUE_".$location) != date("Ymd", time())) {
+
+					setValue("VENUE_".$location, date("Ymd", time()));
+					doCheckin($location);
+
+				}
+
+			}
 
 		}
 
